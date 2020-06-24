5b552c9f26213e86828417ec4dc44ed266edf934
shashank929 <rewaskar.priya@gmail.com>
2020-06-24 12:45:27 +0530

    http Request

--- MyfirstApp/MyfirstApp/Controllers/HomeController.cs
+++ MyfirstApp/MyfirstApp/Controllers/HomeController.cs
@@ -39,5 +39,35 @@ namespace MyfirstApp.Controllers
             var fileName = "PaySlip" + id + ".pdf";
             return File(fileName, "application/pdf");
         }
+
+        public ActionResult StudentDetails()
+        {
+            ViewBag.StudentId = 10;
+            ViewBag.Name = "Geeta";
+            ViewBag.Address = "xy, street number 12,Akola 944002";
+            ViewBag.Marks = 80;
+            ViewBag.Subjects = new List<string> (){ "Physics", "Math", "Chemistry" };
+            return View();
+        }
+
+
+        public ActionResult AmountDemon(int? id)
+        {
+            ViewBag.amount = id;
+            return View();
+        }
+
+
+        public ActionResult RequestExample()
+        {
+            ViewBag.Url = Request.Url;
+            ViewBag.PhysicalApplication = Request.PhysicalApplicationPath;
+            ViewBag.PhysicalPath = Request.PhysicalPath;
+            ViewBag.Headers = Request.Headers["Accept"];
+            ViewBag.QueryString = Request.QueryString["n"];
+            ViewBag.Browser = Request.Browser;
+            ViewBag.HttpMethod =  Request.HttpMethod;
+            return View();
+        }
     }
 }
