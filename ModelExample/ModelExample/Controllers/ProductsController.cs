de60570eb52acd7744855835717000f4d52b95e4
shashank929 <rewaskar.priya@gmail.com>
2020-06-26 03:29:51 +0530

    accessing Model example

---
+++ ModelExample/ModelExample/Controllers/ProductsController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ModelExample.Models;
+
+namespace ModelExample.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product(){ ProductId = 10, ProductName = "Mobiles" , Rate =40000 },
+                new Product(){ ProductId = 11, ProductName = "Bike" , Rate =76000 },
+                new Product(){ ProductId = 12, ProductName = "Headphone" , Rate =9000 },
+                new Product() { ProductId = 13, ProductName = "Speakers" , Rate =10000 }
+            };
+            ViewBag.products = products;
+
+            return View();
+        }
+
+        public ActionResult Details(int? id)
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product(){ ProductId = 10, ProductName = "Mobiles" , Rate =40000 },
+                new Product(){ ProductId = 11, ProductName = "Bike" , Rate =76000 },
+                new Product(){ ProductId = 12, ProductName = "Headphone" , Rate =9000 },
+                new Product(){ ProductId = 13, ProductName = "Speakers" , Rate =10000 }
+            };
+            Product matchingProduct = new Product(); 
+            
+            foreach(var item in products)
+            {
+                if(item.ProductId == id)
+                {
+                    matchingProduct = item;
+                }
+            }
+            ViewBag.ProductDetails = matchingProduct;
+
+            return View(matchingProduct);
+         
+        }
+    }
+}
