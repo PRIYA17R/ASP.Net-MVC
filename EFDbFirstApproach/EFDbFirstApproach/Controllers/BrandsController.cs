f9e275573322ba337204a86a0d15ae766580aeb5
shashank929 <rewaskar.priya@gmail.com>
2020-06-27 04:58:33 +0530

    db firts enity framework program

---
+++ EFDbFirstApproach/EFDbFirstApproach/Controllers/BrandsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EFDbFirstApproach.Models;
+
+namespace EFDbFirstApproach.Controllers
+{
+    public class BrandsController : Controller
+    {
+        // GET: Brands
+        public ActionResult Index()
+        {
+            EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
+            List<Brand> Brands = db.Brands.ToList();
+            return View(Brands);
+          
+        }
+    }
+}
