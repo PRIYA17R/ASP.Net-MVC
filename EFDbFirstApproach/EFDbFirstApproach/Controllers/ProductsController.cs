a602ef3289f09eb4d55f0c6a06c0227551eaf240
shashank929 <rewaskar.priya@gmail.com>
2020-06-27 06:33:26 +0530

    search functionality

--- EFDbFirstApproach/EFDbFirstApproach/Controllers/ProductsController.cs
+++ EFDbFirstApproach/EFDbFirstApproach/Controllers/ProductsController.cs
@@ -11,19 +11,22 @@ namespace EFDbFirstApproach.Controllers
     public class ProductsController : Controller
     {
         // GET: Products
-        public ActionResult Index()
+        public ActionResult Index(string Search = "")
         {
             EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
-            // List<Product> products = db.Products.ToList();
-            //  List<Product> products = db.Products.Where(t => t.CategoryID == 1).ToList();
+           
+            List<Product> products = db.Products.Where(t => t.ProductName.Contains(Search)).ToList();
 
-            SqlParameter[] sqlParameters = new SqlParameter[]
-            {
-                new SqlParameter("@BrandID", 2)
-            };
+            //    SqlParameter[] sqlParameters = new SqlParameter[]
+            //    {
+            //        new SqlParameter("@BrandID", 2)
+            //    };
 
-        List<Product> products = db.Database.SqlQuery<Product>("exec GetProductIdByBrand @BrandID ", sqlParameters).ToList();
-            return View(products);
+            //List<Product> products = db.Database.SqlQuery<Product>("exec GetProductIdByBrand @BrandID ", sqlParameters).ToList();
+
+            ViewBag.searchText = Search;
+
+         return View(products);
         }
     }
 }
